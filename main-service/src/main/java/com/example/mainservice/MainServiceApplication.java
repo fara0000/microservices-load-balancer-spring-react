9f066f971d8dd9a13290ff5361404feda408ed8a
fara0000 <fi@livereach.com>
2023-01-16 00:27:05 +0300

    added

--- main-service/src/main/java/com/example/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/com/example/mainservice/MainServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example.mainservice;
 
+import com.example.mainservice.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
 @EnableSwagger2
+@EnableEurekaClient
+@RibbonClient(name = "main-service", configuration = RibbonConfiguration.class)
 @SpringBootApplication
 public class MainServiceApplication {
 
---
+++ main-service/src/main/java/com/example/mainservice/config/PropertiesConfig.java
@@ -0,0 +1,17 @@
+package com.example.mainservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class PropertiesConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
+
